92cec7f5c9a3cb49957a4e14249b32db9ce5c4e1
RyuJaeChan <rjh221@gmail.com>
2016-05-14 18:19:21 +0900

    edit isCreate

--- OurLogicSimulator/LeftSideOne.cpp
+++ OurLogicSimulator/LeftSideOne.cpp
@@ -5,7 +5,8 @@
 #include "OurLogicSimulator.h"
 #include "LeftSideOne.h"
 #include "OurLogicSimulatorDoc.h"
-#include "Gate.h"
+#include "Gate.h"							////
+#include "MainFrm.h"
 
 
 
@@ -86,8 +87,8 @@ void CLeftSideOne::OnTvnSelchangedTree1(NMHDR *pNMHDR, LRESULT *pResult)
 	
 	CString str = m_treeCtrl.GetItemText(hItem);
 	
-	
-	
+	CMainFrame *pFrame = (CMainFrame *)AfxGetMainWnd();
+	pFrame->m_pMainView->isCreate = m_treeCtrl.GetItemData(pNMTreeView->itemNew.hItem);
 	
 	*pResult = 0;
 }
